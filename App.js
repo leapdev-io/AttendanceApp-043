95ac0ce166822bef80738c8e12558cbcda8224c5
leapdev.io <liplip1818@gmail.com>
2026-03-11 11:58:21 +0700

    Penambahan File AttendanceApp-043

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.warnaMerah} />
+      <View style={styles.warnaBiru} />
+      <View style={styles.warnaHijau} />
+      <View style={styles.warnaOren} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    flexDirection: "row",
+  },
+
+  warnaMerah: {
+    width: 40,
+    height: 20,
+    backgroundColor: "red",
+  },
+
+  warnaBiru: {
+    width: 120,
+    height: 70,
+    backgroundColor: "blue",
+  },
+
+  warnaHijau: {
+    width: 120,
+    height: 100,
+    backgroundColor: "green",
+  },
+
+  warnaOren: {
+    width: 120,
+    height: 150,
+    backgroundColor: "orange",
   },
 });
